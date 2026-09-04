ba1ab5e1c7625a62a9bbfe94ad34d5db3c114ce1
jeh-p-em <50097354+jeh-p-em@users.noreply.github.com>
2026-09-04 00:39:13 -0500

    Update mkv-ultra.sh

--- mkv-ultra.sh
+++ mkv-ultra.sh
@@ -3,7 +3,7 @@
 compress_check=true
 ntfy_id="72c947f5-7ab2-4bc2-b536-8576b998c8a4"
 compression_lvl="24"
-base_temp_dir="/opt/compress_mkv"
+base_temp_dir="~/mkv-ultra"
 temp_dir=""
 source_dir="."
 file_name=""
@@ -29,6 +29,10 @@ Options:
         the amount of data that was saved and the time it took.
         The original source files are not replaced with the commpressed versions.
 
+  -y, --ntfy-id
+        Specify your own ntfy ID string to send updates to.
+        Default: $ntfy_id
+
   -t, --temp-dir DIRECTORY
         Directory to use for temporary files.
         Default: $base_temp_dir
@@ -49,8 +53,8 @@ EOF
 }
 
 OPTIONS=$(getopt \
-	--options hfknt:l:s:x: \
-	--longoptions help,force,keep-temp-files,no-replace,temp-dir:,compression-level:,source-dir:,file-name: \
+	--options hfkny:t:l:s:x: \
+	--longoptions help,force,keep-temp-files,no-replace,ntfy-id:,temp-dir:,compression-level:,source-dir:,file-name: \
 	--name "$0" \
 	-- "$@"
 )
@@ -79,6 +83,10 @@ while true; do
 		-n|--no-replace)
 			no_replace=true
 			shift
+			;;
+		-y|--ntfy-id)
+			ntfy_id="$2"
+			shift 2
 			;;
 		-t|--temp-dir)
 			base_temp_dir="$2"
@@ -121,7 +129,7 @@ get_files() {
 		fi
 		printf '%s\0' "$found_file"
 	else
-		find "$source_dir" -type f \( -iname "*.mkv" -o -iname "*.mp4" \) -print0 | sort -zV
+		find "$source_dir" -type f \( -iname "*.mkv" -o -iname "*.mp4" -o -iname "*.avi" \) -print0 | sort -zV
 	fi
 }
 
@@ -182,31 +190,32 @@ while IFS= read -r -d '' source_file; do
 
 	echo "Processing: $source_file"
 	basename_file=$(basename "$source_file")
+	final_file="${source_file%.*}.mkv"
 	ext="${source_file##*.}"
 	ext="${ext,,}"
 
-	tmp_file=$(mktemp --suffix=".$ext" "$temp_dir"/ffmpeg.XXXXXX)
-	output_file=$(mktemp --suffix=".$ext" "$temp_dir"/output.XXXXXX)
+	temp_file=$(mktemp --suffix=".$ext" "$temp_dir"/ffmpeg.XXXXXX)
+	output_file=$(mktemp --suffix=".mkv" "$temp_dir"/output.XXXXXX)
 	attach_dir=$(mktemp -d "$temp_dir"/attachments.XXXXXX)
 
-	echo "Copying: $source_file to $tmp_file"
-	if ! cp -- "$source_file" "$tmp_file"; then
+	echo "Copying: $source_file to $temp_file"
+	if ! cp -- "$source_file" "$temp_file"; then
 		echo "Failed to copy $source_file"
 		cleanup
 		continue
 	fi
 
-	before_size=$(stat -c %s "$tmp_file")
+	before_size=$(stat -c %s "$temp_file")
 	before_size_mb="$((before_size / 1024 / 1024))MB"
 
 	if [[ "$ext" == "mkv" ]]; then
 		echo "Checking attachments..."
-		attachment_count=$(mkvmerge -J "$tmp_file" | jq '.attachments | length')
+		attachment_count=$(mkvmerge -J "$temp_file" | jq '.attachments | length')
 
 		if [[ "$attachment_count" -gt 0 ]]; then
 			echo "Extracting $attachment_count attachments..."
 			while IFS=: read -r id name; do
-				if ! mkvextract -q attachments "$tmp_file" \
+				if ! mkvextract -q attachments "$temp_file" \
 					"$id:$attach_dir/$name"
 				then
 					echo "Failed to extract attachment: $name"
@@ -214,7 +223,7 @@ while IFS= read -r -d '' source_file; do
 					continue 2
 				fi
 			done < <(
-				mkvmerge -J "$tmp_file" |
+				mkvmerge -J "$temp_file" |
 					jq -r '.attachments[] | "\(.id):\(.file_name)"'
 			)
 		else
@@ -224,7 +233,37 @@ while IFS= read -r -d '' source_file; do
 		attachment_count=0
 	fi
 
-	echo "Encoding: $tmp_file to $output_file"
+	source_pix_fmt=$(ffprobe -v error \
+		-select_streams v:0 \
+		-show_entries stream=pix_fmt \
+		-of default=noprint_wrappers=1:nokey=1 \
+		"$temp_file")
+
+	case "$source_pix_fmt" in
+		yuv420p)
+			format="nv12"
+			profile="main"
+			;;
+		yuv420p10le)
+			format="p010le"
+			profile="main10"
+			;;
+		yuv422p10le)
+			format="yuv422p10le"
+			profile="rext"
+			;;
+		yuv444p10le)
+			format="yuv444p10le"
+			profile="rext"
+			;;
+		*)
+			echo "Unsupported pixel format: $source_pix_fmt"
+			cleanup
+			continue
+			;;
+	esac
+
+	echo "Encoding: $temp_file to $output_file"
 	if ffmpeg \
 		-hide_banner \
 		-v error \
@@ -233,9 +272,10 @@ while IFS= read -r -d '' source_file; do
 		-probesize 100M \
 		-analyzeduration 100M \
 		-vaapi_device /dev/dri/renderD128 \
-		-i "$tmp_file" \
-		-filter_complex "[0:v:0]format=nv12,hwupload[v]" \
+		-i "$temp_file" \
+		-filter_complex "[0:v:0]hqdn3d=0.8:0.8:3:3,format=$format,hwupload[v]" \
 		-map "[v]" \
+		-profile:v:0 "$profile" \
 		-map 0:a? \
 		-map 0:s? \
 		-map 0:v:1? \
@@ -246,9 +286,7 @@ while IFS= read -r -d '' source_file; do
 		-rc_mode:v:0 ICQ \
 		-qp:v:0 "$compression_lvl" \
 		-c:v:1 copy \
-		-c:a libopus \
-		-b:a 128k \
-		-ac 2 \
+		-c:a copy \
 		-c:s copy \
 		"$output_file"
 
@@ -281,6 +319,7 @@ while IFS= read -r -d '' source_file; do
 		if [[ "$no_replace" == false ]]; then
 			echo "Copying: $output_file to $source_file"
 			if cp -f -- "$output_file" "$source_file"; then
+				mv -- "$source_file" "$final_file"
 				elapsed=$(elapsed_time)
 				ntfy "File Compressed: $(ntfy_data)"
 			else
