ed67f44878a61b1d64cf529ccb7f02e4a3e98726
jeh-p-em <50097354+jeh-p-em@users.noreply.github.com>
2026-09-07 23:19:16 -0500

    Update mkv-ultra.sh

--- mkv-ultra.sh
+++ mkv-ultra.sh
@@ -232,6 +232,8 @@ cleanup() {
 	if [[ "$keep_temp_files" == false ]]; then
 		echo "Deleting temporary files."
 		rm -rf -- "$temp_dir"/*
+	else
+		echo "Temporary files kept in $temp_dir"
 	fi
 }
 
@@ -447,7 +449,6 @@ while IFS= read -r -d '' source_file; do
 				)
 				;;
 		esac
-
 		((audio_index++))
 	done < <(
 		ffprobe -v error \
@@ -468,7 +469,7 @@ while IFS= read -r -d '' source_file; do
 		-analyzeduration 100M \
 		-vaapi_device /dev/dri/renderD128 \
 		-i "$temp_file" \
-		-filter_complex "[0:v:0]format=$format,hwupload[v]" \
+		-filter_complex "[0:v:0]hqdn3d=0.8:0.8:3:3,format=$format,hwupload[v]" \
 		-map "[v]" \
 		-profile:v:0 "$profile" \
 		-map 0:a? \
